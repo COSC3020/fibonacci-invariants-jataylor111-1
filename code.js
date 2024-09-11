b6e7390ae641cbf2bcc64ba5f097c1cf59228536
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2024-09-11 15:28:39 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,4 @@
 function fib(n) {
-    if (n == 0) {
-        return [0];
-    }
-    else if (n == 1) {
-        return [0,1];
-    }
-    else {
-        let arr = [0,1];
-        for (var i = 2; i <= n; i++) {
-            arr[i] = arr[i-1] + arr[i-2];
-        }
-        return arr;
-    }
-
     return n;
 }
 
